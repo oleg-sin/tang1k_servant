ea0dd2efd0c2593da4df39642517ea4676a209ff
Oleg Libin <oleg.sin@icloud.com>
2025-05-05 13:32:14 +0500

    Update blinky_c.c

--- src/sw/blinky_c.c
+++ src/sw/blinky_c.c
@@ -4,9 +4,7 @@ typedef struct {
     volatile uint32_t DATA;
 } GPIO_Type;
 
-#define GPIO_BASE 0x40000000
-
-GPIO_Type * const GPIO __attribute__((section(".gpio_registers"))) = (GPIO_Type *)GPIO_BASE;
+#define GPIO_BASE *((volatile  unsigned int*)0x40000000)
 
 void shitty_delay(void) {
     for(volatile uint32_t i = 0; i < 50000; i++);
@@ -14,7 +12,9 @@ void shitty_delay(void) {
 
 void _start(void) {
     while(1) {
-        GPIO->DATA ^= (1 << 0);
+        GPIO_BASE = 1;
+        shitty_delay();
+        GPIO_BASE = 0;
         shitty_delay();
     }
 }
